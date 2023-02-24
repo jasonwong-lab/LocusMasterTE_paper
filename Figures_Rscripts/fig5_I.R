c76e2997b71dd6bb15c0b410b69d410b1b646415
LEESojung1998 <80635289+LEESojung1998@users.noreply.github.com>
2023-02-24 15:00:18 +0800

    Update fig5_I.R

--- Figures_Rscripts/fig5_I.R
+++ Figures_Rscripts/fig5_I.R
@@ -3,19 +3,11 @@ My_Theme = theme(legend.text = element_text(size=10), legend.title = element_bla
                  axis.text.x = element_text(colour = "black",size = 10),
                  axis.title.y = element_text(colour = "black",size = 10),axis.text.y = element_text(colour = "black",size = 10),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(),axis.line = element_line(colour = "black"))
 
-combined_raw <- read.delim("https://figshare.com/ndownloader/files/39038678", comment.char="#")
-hg38_gencode_rmsk_indi_loc_annotate <- read.delim("https://figshare.com/ndownloader/files/39052802", header=FALSE)
-
-## From Spanki select only TP from lasTEq
-tp_org_tel <- combined_raw[which(combined_raw$Long!=0 & combined_raw$org_tel!=0),1:2]
-tp_mod_tel <- combined_raw[which(combined_raw$Long!=0 & combined_raw$mod_tel!=0),1:2]
-only_mod <- tp_mod_tel[!tp_mod_tel$TE_name %in% tp_org_tel$TE_name,]
-
-only_mod$loc <- unlist(hg38_gencode_rmsk_indi_loc_annotate[match(only_mod$TE_name,hg38_gencode_rmsk_indi_loc_annotate$V4),5])
-
-### Extract tumor sample only
-TCGA_COAD_TE_exp <- get(load("https://figshare.com/ndownloader/files/39052376"))
-only_mod_imp <- only_mod[only_mod$loc != "exonic",]
+### prepare_extraction
+TCGA_COAD_TE_exp <- get(load("https://figshare.com/ndownloader/files/39384629"))
+diff_TE <- read.delim("https://figshare.com/ndownloader/files/39384632")
+diff_TE <- diff_TE[diff_TE$diff > 0.1,]
+only_mod_imp <- diff_TE[diff_TE$loc != "exonic",]
 TCGA_COAD_TE_exp_nec_no0 <- TCGA_COAD_TE_exp[rownames(TCGA_COAD_TE_exp) %in% only_mod_imp$TE_name,]
 TCGA_COAD_TE_exp_nec_no0 <- TCGA_COAD_TE_exp_nec_no0[,str_detect(colnames(TCGA_COAD_TE_exp_nec_no0),"01A")]
 colnames(TCGA_COAD_TE_exp_nec_no0) <- substr(colnames(TCGA_COAD_TE_exp_nec_no0),1,12)
@@ -30,8 +22,8 @@ survival_four_raw <- survival_four_raw[survival_four_raw$OS.time != 0,]
 TCGA_COAD_TE_exp_nec_no0 <- TCGA_COAD_TE_exp_nec_no0[,colnames(TCGA_COAD_TE_exp_nec_no0) %in% survival_four_raw$X_PATIENT]
 TCGA_COAD_TE_exp_nec_no0 <- data.frame(TCGA_COAD_TE_exp_nec_no0)
 
-## only seelect TE with 80% expressed
-threshold <- ncol(TCGA_COAD_TE_exp_nec_no0) * 0.2
+## only select TE with 80% expressed
+threshold <- ncol(TCGA_COAD_TE_exp_nec_no0) * 0.8
 TCGA_COAD_TE_exp_nec_no0 <- TCGA_COAD_TE_exp_nec_no0[rowSums(TCGA_COAD_TE_exp_nec_no0 == 0) <= threshold, ]
 survival_four_raw <- survival_four_raw[survival_four_raw$X_PATIENT %in% colnames(TCGA_COAD_TE_exp_nec_no0),]
 for(i in 1:nrow(TCGA_COAD_TE_exp_nec_no0)){
@@ -39,25 +31,28 @@ for(i in 1:nrow(TCGA_COAD_TE_exp_nec_no0)){
 }
 colnames(survival_four_raw)[12:ncol(survival_four_raw)] <- rownames(TCGA_COAD_TE_exp_nec_no0)
 
+survival_four_raw <- survival_four_raw[survival_four_raw$PFI.time > 0,]
+
 te <- survival_four_raw[,12:ncol(survival_four_raw)]
 te <- as.matrix(sapply(te, as.numeric))  
 
-### Look at OS and PFI ###
-## OS ##
+### Look at PFI ###
+## PFI ##
 for(i in c(1:100)){
   cv.fit2 = cv.glmnet(te,
-                      Surv(survival_four_raw$OS.time, survival_four_raw$OS),
+                      Surv(survival_four_raw$PFI.time, survival_four_raw$PFI),
                       alpha = 1,
                       family = "cox")
   small.lambda.index <- which(cv.fit2$lambda == cv.fit2$lambda.min)
   small.lambda.betas <- cv.fit2$glmnet.fit$beta[,small.lambda.index]
   small.lambda.betas <- data.frame(small.lambda.betas)
   small.lambda.betas <- rownames(small.lambda.betas)[small.lambda.betas$small.lambda.betas !=0]
+  print(small.lambda.betas)
 }
 
-### Selected 5 TEs
-tot <- c("AluSp_47159","MIRb_187527","PABL_A-int_27","AluSp_9954","AluYe5_1025")
 
+### Selected TE
+tot <- c("LSU-rRNA_Hsa_401","THE1C_758")
 ### find optimal clusters ###
 library(ConsensusClusterPlus)
 TCGA_COAD_TE_extract <- TCGA_COAD_TE_exp_nec_no0[rownames(TCGA_COAD_TE_exp_nec_no0) %in% tot,]
@@ -69,9 +64,9 @@ temp <- data.frame(results[[2]]$consensusClass)
 survival_four <- survival_four_raw[,c(1:10)]
 survival_four$TE_group <- unlist(temp[match(survival_four$X_PATIENT,rownames(temp)),1])
 
-## total 150 TEs --> FDR < 0.05, P-value < 0.05
-## OS ##
-fit <- survfit(Surv(OS.time, OS) ~ TE_group, data = survival_four)
+## PFI ##
+fit <- survfit(Surv(PFI.time, PFI) ~ TE_group, data = survival_four)
+
 library(survminer)
 ggsurvplot(fit,
            pval = TRUE,
@@ -79,11 +74,4 @@ ggsurvplot(fit,
            risk.table.col = "strata", 
            linetype = "strata",
            palette = c("#E7B800", "#2E9FDF"),
-           ggtheme = My_Theme,xlab = "OS(time)")
-
-### functionally + location annotate TEs ###
-tot <- data.frame(tot)
-tot$loc <- unlist(hg38_gencode_rmsk_indi_loc_annotate[match(tot$tot,hg38_gencode_rmsk_indi_loc_annotate$V4),5])
-tot$chr <- unlist(hg38_gencode_rmsk_indi_loc_annotate[match(tot$tot,hg38_gencode_rmsk_indi_loc_annotate$V4),1])
-tot$start <- unlist(hg38_gencode_rmsk_indi_loc_annotate[match(tot$tot,hg38_gencode_rmsk_indi_loc_annotate$V4),2])
-tot$end <- unlist(hg38_gencode_rmsk_indi_loc_annotate[match(tot$tot,hg38_gencode_rmsk_indi_loc_annotate$V4),3])
+           ggtheme = My_Theme,xlab = "PFI(time)")
